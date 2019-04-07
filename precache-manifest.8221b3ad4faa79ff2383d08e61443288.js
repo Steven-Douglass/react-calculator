8b488aa9e837c5fa197e5714a2a7a85b0fed478a
Steve940 <smd940@gmail.com>
2019-04-07 12:01:10 -0400

    Updates

---
+++ precache-manifest.8221b3ad4faa79ff2383d08e61443288.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2db6eb070a89d29d683a",
+    "url": "/react-calculator/static/css/main.53aee49e.chunk.css"
+  },
+  {
+    "revision": "2db6eb070a89d29d683a",
+    "url": "/react-calculator/static/js/main.473ddd69.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "60b35a1e1ed30e08969e",
+    "url": "/react-calculator/static/js/2.500deffa.chunk.js"
+  },
+  {
+    "revision": "d8cc1abc59b842d0d019ec743f43623d",
+    "url": "/react-calculator/index.html"
+  }
+];
